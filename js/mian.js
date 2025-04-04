32fb2f0a69a90eba1f3681271bd6cf5ad2311bc7
JTcompNinfo <cte-joel.dinovo@sllboces.org>
2025-04-04 01:13:24 +0000

    DONE

--- js/mian.js
+++ js/mian.js
@@ -5,3 +5,15 @@ hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
 })
+
+$(".click-me1").click(function(){
+    $(".hide-me1").slideToggle("slow");
+});
+
+$(".click-me2").click(function(){
+    $(".hide-me2").slideToggle("slow");
+});
+
+$(".click-me3").click(function(){
+    $(".hide-me3").slideToggle("slow");
+});
